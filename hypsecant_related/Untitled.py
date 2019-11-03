95868a3d26df1ca38e376a0efed772196e09b9ee
Fumito Nakamura <ffumituchi@gmail.com>
2019-11-03 11:27:54 +0900

    little fixes
    little fixes

---
+++ hypsecant_related/Untitled.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %matplotlib inline
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(start = -10, stop = 10, num = 100)
+fx = 1/(2*np.pi) / (np.cosh(x/2))
+gx = 1/(np.sqrt(2*np.pi))*np.exp(-x**2/2)
+
+plt.plot(x,gx, lw=7, label = "Gaussian Dist")
+plt.plot(x,fx, lw=7, label="HSD")
+plt.legend()
+plt.savefig("GD_HSD.png")
+plt.show()
